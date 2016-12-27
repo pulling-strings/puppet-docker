9c789a7076695adcde1fe316df643436d70b2916
narkisr <narkisr@gmail.com>
2016-12-27 23:01:24 +1100

    initial import

---
+++ manifests/init.pp
@@ -0,0 +1,21 @@
+# A module for setting up Docker on Ubuntu
+class docker {
+  apt::key {'docker':
+    id     => '58118E89F3A912897C070ADBF76221572C52609D',
+    server => 'hkp://ha.pool.sks-keyservers.net:80',
+  } ->
+
+  apt::source { 'docker':
+      location => 'https://apt.dockerproject.org/repo',
+      release  => 'ubuntu-xenial',
+      repos    => 'main',
+      include  => {
+        src => false,
+      }
+  } ->
+
+  package{'docker-engine':
+    ensure  => present,
+    require => Class['apt::update']
+  }
+}
