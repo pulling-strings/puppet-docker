cc4c3d3d8f7e2cba6eb5b0392bf8930940f48a54
ronen <narkisr@gmail.com>
2017-01-08 21:35:09 +1100

    adding timeout

--- manifests/image.pp
+++ manifests/image.pp
@@ -1,9 +1,12 @@
 # Installing a docker image
-define docker::image {
+define docker::image(
+  $timeout = 120
+){
   exec{"pull ${name}":
     command => "docker pull ${name}",
     user    => 'root',
     path    => ['/usr/bin','/bin',],
+    timeout => $timeout,
     require => Package['docker-engine']
   }
 }
